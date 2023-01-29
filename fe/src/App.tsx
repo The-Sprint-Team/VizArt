f3edf5c4274be81144573adca9e9638d108f9e86
Jingxiang Mo <65676392+jingxiangmo@users.noreply.github.com>
2023-01-29 09:53:36 -0500

    Hot fix

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <Route path="create" element={<Create />} />
           <Route
             path="compete"
-            element={<Create forcedTitle="Face Competition" />}
+            element={<Create forcedTitle="Tree 🌲 Competition" />}
           />
           <Route path="explore/:uid" element={<Share />} />
           <Route path="explore" element={<Explore />} />
