9dcacf7c96d4addce3839aef03dfbf19b33b19f1
Anton Otaner <antonethem@gmail.com>
2023-01-28 23:49:08 -0500

    add explore and share page

--- fe/src/components/Header/Header.tsx
+++ fe/src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import styles from "./style.module.scss";
 import Button from "../Button/Button";
 
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation, Link } from "react-router-dom";
 import { useEffect } from "react";
 
 import logo from "../../assets/logo.png";
@@ -22,8 +22,9 @@ export default function Header() {
 
   return (
     <div className={styles.header}>
-      <img src={logo} alt="Logo" className={styles.logo} />
-
+      <Link to="/explore">
+        <img src={logo} alt="Logo" className={styles.logo} />
+      </Link>
       <div>
         <Button
           name="Create"
